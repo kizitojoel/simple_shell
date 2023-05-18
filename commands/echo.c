d82e1e3e47e6b2089fc421ab5aa1f10379f72e78
John Gaitho <johngaitho05@gmail.com>
2023-05-18 11:28:15 +0300

    implement echo

---
+++ commands/echo.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - displays a line of text
+ * @argc: number of args
+ * @argv: list of args
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+    int i;
+
+    for (i = 1; i < argc; i++)
+    {
+        printf("%s", argv[i]);
+        if(i < argc-1)
+            printf(" ");
+    }
+    printf("\n");
+    return (0);
+}
