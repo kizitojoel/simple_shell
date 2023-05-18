b33d8f9ed8d825cbaf3415ca0b9ba0e1a5897700
John Gaitho <johngaitho05@gmail.com>
2023-05-18 12:49:27 +0300

    implement touch

--- commands/touch.c
+++ commands/touch.c
@@ -1,3 +1,26 @@
-//
-// Created by yk on 5/18/23.
-//
+#include "main.h"
+
+/**
+ * main - create empty file(s) with the passed name(s)
+ * @argc: number of args
+ * @argv: list of args
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc < 2)
+	{
+	printf("touch: missing file operand");
+	return (-1);
+	}
+	chdir(_getcwd());
+	for (i = 0; i < argc; i++)
+	{
+		open(argv[i], O_CREAT | O_RDWR | O_TRUNC);
+	}
+
+	return (0);
+
+}
